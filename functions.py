b0cec1f56a042b115804f5f495b3e088e6541c21
caleb jephuneh <calebjephunneh@gmail.com>
2023-03-25 01:36:21 +0300

    python

--- functions.py
+++ functions.py
@@ -1,4 +1,13 @@
 def greetings_function():
-    print ('welcome user')
+    print ('welcome user to here')
 
 greetings_function()
+
+
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+num1 = int(input( 'input the first number:'))
+num2 = int(input( 'input the second number:'))
+print (add_numbers(num1, num2))
