67c6ce504a662c8191346505399be699bdcc6394
caleb jephuneh <calebjephunneh@gmail.com>
2023-03-25 01:50:57 +0300

    xode

---
+++ if.py
@@ -0,0 +1,25 @@
+a = 4
+b = 3
+
+if a == b:
+    print('A equals B')
+
+else:
+    print('A not equals B')
+
+
+
+
+value = input( 'Enter a value')
+
+if type(value) == str:
+    print('Value is string')
+elif type(value) == int:
+    print('value is an interger')
+elif type(value) == bool:
+    print('Value is a Booloon')
+
+else:
+    print('There is no such ')
+
+
