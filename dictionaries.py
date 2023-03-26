29d4f0b54af6c361893483703e14a9e3d640af33
caleb jephuneh <calebjephunneh@gmail.com>
2023-03-27 00:31:42 +0300

    more

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+my_dict = {
+    'name' : 'Tim',
+    'Nationality': 'Kenyan',
+    'Qualification' : "Highschool Diploma"
+}
+
+print(my_dict['Qualification'])
---
+++ forloops.py
@@ -0,0 +1,6 @@
+mylist = ['caleb', 'jephuneh', 'ombori', 'your ','mother','is','at', 'the ', 'gate']
+
+for letter in mylist:
+    print(letter)
+    if letter == 'ombori':
+        break
