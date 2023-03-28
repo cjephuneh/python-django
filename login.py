0859cc1adcb585c9ad26be4433660e1482899584
caleb jephuneh <calebjephunneh@gmail.com>
2023-03-28 19:38:54 +0300

    code

---
+++ login.py
@@ -0,0 +1,17 @@
+print('Create Account')
+Username = input('Input username: ')
+Password = input('Input Password: ')
+
+print ('Your account has been created successfully')
+
+print('Login now')
+
+Username1 = input('Enter Username: ')
+Password2 = input('Enter Password: ')
+
+if Username == Username1 and Password == Password2:
+    print ('Login successful')
+else:
+    print ('Invalid username or password')
+    
+
